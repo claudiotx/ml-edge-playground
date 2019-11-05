21139e673a844baabf6440229073ef7b82eb8217
Claudio Teixeira <claudioteixeira7@gmail.com>
2019-11-05 15:12:33 +0000

    feat(help): show modal for help with command

---
+++ README.md
@@ -0,0 +1,7 @@
+## ML on the Edge
+Official Docs:
+https://github.com/tensorflow/tfjs-models/tree/master/speech-commands
+
+### Automated Tranfer Learning
+// Serialize Samples
+
--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,7 @@
 let baseRecognizer;
 let recognizer;
 const NUM_FRAMES = 3;
+const SAMPLES = 1;
 let examples = [];
 const INPUT_SHAPE = [NUM_FRAMES, 232, 1];
 let model;
@@ -28,6 +29,22 @@ function setReady() {
   updateConsole('ready!');
 }
 
+function downloadSamples(transferRecognizer) {
+  const artifacts = transferRecognizer.serializeExamples();
+  const anchor = document.createElement('a');
+  anchor.download = `samples.bin`;
+  anchor.href = window.URL.createObjectURL(
+      new Blob([artifacts], {type: 'application/octet-stream'}));
+  anchor.click();
+};
+
+function showHelp() {
+  document.querySelector('#help').className = 'help sans-serif absolute ct pa4 flex justify-center items-center show';
+  setTimeout(() => {
+    document.querySelector('#help').className = 'help sans-serif absolute ct pa4 flex justify-center items-center hide';
+  }, 7e3);
+};
+
 async function collectSamples(recognizer, name, count) {
   for (let index = 0; index < count; index++) {
     updateConsole(`Say ${name}, it: ${index+1}...`);
@@ -36,17 +53,29 @@ async function collectSamples(recognizer, name, count) {
   updateConsole('Ok Thanks');
 }
 
+async function loadSamplesBinaryArrayBuffer(transferRecognizer) {
+  const response = await fetch('samples/samples.bin');
+  const buffer = await response.arrayBuffer();
+  transferRecognizer.loadExamples(buffer);
+}
+
 async function createHelpTransferRecognizer() {
   updateConsole('Creating Transfer Recognizer...');
   const transferRecognizer = baseRecognizer.createTransfer('help'); // 1s
-  updateConsole(`TraCollecting Samples...`);
-  await collectSamples(transferRecognizer, 'help', 3);
-  await collectSamples(transferRecognizer, 'checkout', 3);
-  await collectSamples(transferRecognizer, '_background_noise_', 3);
+
+  // -------------------------------------------------------
+  // Collect Samples
+  updateConsole(`Collecting Samples...`);
+  await collectSamples(transferRecognizer, 'help', SAMPLES);
+  await collectSamples(transferRecognizer, 'checkout', SAMPLES);
+  await collectSamples(transferRecognizer, '_background_noise_', SAMPLES);
   const countExamples = transferRecognizer.countExamples();
   const allExamplesKeys = Object.keys(countExamples);
   const example1 = countExamples[allExamplesKeys[0]];
   updateConsole(`Total Samples Collected ${example1}`);
+
+  // -------------------------------------------------------
+  // Train
   updateConsole(`Training...`);
   await transferRecognizer.train({
     epochs: 10,
@@ -57,16 +86,20 @@ async function createHelpTransferRecognizer() {
     }
   });
   updateConsole(`Training finished.`);
+  downloadSamples(transferRecognizer);
+
+  // -------------------------------------------------------
+  // Listen
   updateConsole(`Listening..`);
   await transferRecognizer.listen(result => {
     let { scores } = result;
     const words = transferRecognizer.wordLabels();
-    scoresList = Array.from(scores).map((s, i) => ({
+    let scoresList = Array.from(scores).map((s, i) => ({
       score: s,
       word: words[i]
     }));
     scoresList.sort((s1, s2) => s2.score - s1.score);
-    updateConsole('Highest Score:', scoresList[0].word, scoresList[0].score);
+    updateConsole(`Highest Score: ${scoresList[0].word}, ${scoresList[0].score}`);
   }, {
     probabilityThreshold: 0.75
   });
@@ -78,6 +111,59 @@ async function createHelpTransferRecognizer() {
   }, 10e3);
 }
 
+async function loadHelpTransferRecognizer() {
+  updateConsole('Creating transfer...');
+  const transferRecognizer = baseRecognizer.createTransfer('help'); // 1s
+
+  // -------------------------------------------------------
+  // Load Samples
+  updateConsole(`Loading samples...`);
+  await loadSamplesBinaryArrayBuffer(transferRecognizer);
+  const exampleCounts = transferRecognizer.countExamples();
+  updateConsole(`Loading finished... ${JSON.stringify(exampleCounts)}`);
+
+  // -------------------------------------------------------
+  // Train
+  updateConsole(`Training...`);
+  await transferRecognizer.train({
+    epochs: 10,
+    callback: {
+      onEpochEnd: async (epoch, logs) => {
+        updateConsole(`Epoch ${epoch}: loss=${logs.loss}, accuracy=${logs.acc}`);
+      }
+    }
+  });
+  updateConsole(`Training finished.`);
+
+  // -------------------------------------------------------
+  // Listen
+  updateConsole(`Listening..`);
+  showHelp();
+  await transferRecognizer.listen(result => {
+    let { scores } = result;
+    const words = transferRecognizer.wordLabels();
+    let scoresList = Array.from(scores).map((s, i) => ({
+      score: s,
+      word: words[i]
+    }));
+    scoresList.sort((s1, s2) => s2.score - s1.score);
+    updateConsole(`Highest Score: ${scoresList[0].word}, ${scoresList[0].score}`);
+    if (scoresList[0].word === 'help') {
+      showHelp();
+    }
+  }, {
+    probabilityThreshold: 0.75
+  });
+  // Stop the recognition in 10 seconds.
+  setTimeout(() => {
+    transferRecognizer.stopListening();
+    updateConsole(`Stopped Listening.`);
+    updateConsole(`Terminated.`);
+  }, 60e3);
+}
+
+
+
 // ------------------------------------------------
 // Initializer Function
 function init() {
@@ -86,7 +172,8 @@ function init() {
     baseRecognizer = speechCommands.create('BROWSER_FFT');
     await baseRecognizer.ensureModelLoaded();
     setReady();
-    await createHelpTransferRecognizer();
+    // await createHelpTransferRecognizer();
+    await loadHelpTransferRecognizer();
   }
   app();
 }
@@ -96,142 +183,3 @@ document.addEventListener('readystatechange', function () {
     init();
   }
 });
-
-
-
-// Deprecated
-// Core Functions
-// async function moveSlider(labelTensor) {
-//   const label = (await labelTensor.data())[0];
-//   document.getElementById('console').textContent = label;
-//   if (label == 2) {
-//     return;
-//   }
-//   let delta = 0.1;
-//   const prevValue = +document.getElementById('output').value;
-//   document.getElementById('output').value =
-//     prevValue + (label === 0 ? -delta : delta);
-// }
-
-// function listen() {
-//   if (recognizer.isListening()) {
-//     recognizer.stopListening();
-//     toggleButtons(true);
-//     document.getElementById('listen').textContent = 'Listen';
-//     return;
-//   }
-//   toggleButtons(false);
-//   document.getElementById('listen').textContent = 'Stop';
-//   document.getElementById('listen').disabled = false;
-
-//   recognizer.listen(async ({
-//     spectrogram: {
-//       frameSize,
-//       data
-//     }
-//   }) => {
-//     const vals = normalize(data.subarray(-frameSize * NUM_FRAMES));
-//     const input = tf.tensor(vals, [1, ...INPUT_SHAPE]);
-//     const probs = model.predict(input);
-//     const predLabel = probs.argMax(1);
-//     await moveSlider(predLabel);
-//     tf.dispose([input, probs, predLabel]);
-//   }, {
-//     overlapFactor: 0.999,
-//     includeSpectrogram: true,
-//     invokeCallbackOnNoiseAndUnknown: true
-//   });
-// }
-
-// function normalize(x) {
-//   const mean = -100;
-//   const std = 10;
-//   return x.map(x => (x - mean) / std);
-// }
-
-// function collect(label) {
-//   console.log('Collecting samples...');
-//   if (recognizer.isListening()) {
-//     return recognizer.stopListening();
-//   }
-//   if (label == null) {
-//     return;
-//   }
-//   recognizer.listen(async ({
-//     spectrogram: {
-//       frameSize,
-//       data
-//     }
-//   }) => {
-//     let vals = normalize(data.subarray(-frameSize * NUM_FRAMES));
-//     examples.push({
-//       vals,
-//       label
-//     });
-//     document.querySelector('#console').textContent =
-//       `${examples.length} examples collected`;
-//   }, {
-//     overlapFactor: 0.999,
-//     includeSpectrogram: true,
-//     invokeCallbackOnNoiseAndUnknown: true
-//   });
-// }
-
-// async function train() {
-//   toggleButtons(false);
-//   const ys = tf.oneHot(examples.map(e => e.label), 3);
-//   const xsShape = [examples.length, ...INPUT_SHAPE];
-//   const xs = tf.tensor(flatten(examples.map(e => e.vals)), xsShape);
-
-//   await model.fit(xs, ys, {
-//     batchSize: 16,
-//     epochs: 10,
-//     callbacks: {
-//       onEpochEnd: (epoch, logs) => {
-//         document.querySelector('#console').textContent =
-//           `Accuracy: ${(logs.acc * 100).toFixed(1)}% Epoch: ${epoch + 1}`;
-//       }
-//     }
-//   });
-//   tf.dispose([xs, ys]);
-//   toggleButtons(true);
-// }
-
-// /*
-//   Creates a Sequential Model
-//  */
-// function buildModel() {
-//   model = tf.sequential();
-//   model.add(tf.layers.depthwiseConv2d({
-//     depthMultiplier: 8,
-//     kernelSize: [NUM_FRAMES, 3],
-//     activation: 'relu',
-//     inputShape: INPUT_SHAPE
-//   }));
-//   model.add(tf.layers.maxPooling2d({
-//     poolSize: [1, 2],
-//     strides: [2, 2]
-//   }));
-//   model.add(tf.layers.flatten());
-//   model.add(tf.layers.dense({
-//     units: 3,
-//     activation: 'softmax'
-//   }));
-//   const optimizer = tf.train.adam(0.01);
-//   model.compile({
-//     optimizer,
-//     loss: 'categoricalCrossentropy',
-//     metrics: ['accuracy']
-//   });
-// }
-
-// function toggleButtons(enable) {
-//   // document.querySelectorAll('a').forEach(b => a.disabled = !enable);
-// }
-
-// function flatten(tensors) {
-//   const size = tensors[0].length;
-//   const result = new Float32Array(tensors.length * size);
-//   tensors.forEach((arr, i) => result.set(arr, i * size));
-//   return result;
-// }
